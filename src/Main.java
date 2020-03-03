b39be801797fb71d442dea3e5f6735590bc7a934
Natalia Bryzgalova <bryzgalova_n@mail.ru>
2020-03-03 16:59:14 +0500

    change

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
         double currentaccount = 100;
         double endaccount;
-        double added=1050;
-        if (added>=1000) {
-            endaccount = currentaccount+added+added*0.01;
+        double added = 1050;
+        if (added >= 1000) {
+            endaccount = currentaccount + added + added * 0.01;
         } else {
-            endaccount=currentaccount+added;
+            endaccount = currentaccount + added;
         }
         System.out.println(endaccount);
     }
